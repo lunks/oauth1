65c375bf3a6dda1e7d8c1703b7018dc9b724ea37
Israel Ribeiro <israveri@gmail.com>
2014-05-16 12:17:11 -0300

    Updates specs to use expect syntax

--- spec/oauth1/helper_spec.rb
+++ spec/oauth1/helper_spec.rb
@@ -1,66 +1,66 @@
 require 'spec_helper'
 
 describe OAuth1::Helper do
-  let(:url) { "http://example.com" }
-  let(:params) { {my_special_param: "params"} }
-  let(:consumer_key) { "some_key" }
-  let(:consumer_secret) { "some_secret" }
+  let(:url) { 'http://example.com' }
+  let(:params) { {user_specified_param: 'params'} }
+  let(:consumer_key) { 'some_key' }
+  let(:consumer_secret) { 'some_secret' }
 
   let(:helper) { OAuth1::Helper.new(:get, url, params, {consumer_key: consumer_key, consumer_secret: consumer_secret}) }
 
-  describe "#url_params" do
+  describe '#url_params' do
     let(:url_params) { helper.url_params }
-    it "prepends oauth_ to option keys" do
-      url_params.keys.should include(:oauth_consumer_key)
+
+    it 'prepends "oauth_" to option keys' do
+      expect(url_params.keys).to include(:oauth_consumer_key)
     end
 
-    it "has some default options" do
-      url_params.keys.should include(:oauth_version)
+    it 'has some default options' do
+      expect(url_params.keys).to include(:oauth_version)
     end
 
-    it "doesn't include the consumer_secret" do
-      url_params.keys.should_not include("oauth_consumer_secret")
+    it 'does not include the consumer_secret' do
+      expect(url_params.keys).to_not include('oauth_consumer_secret')
     end
 
-    it "has oauth_token as an empty string" do
-      url_params[:oauth_token].should eq("")
+    it 'has oauth_token as an empty string' do
+      expect(url_params[:oauth_token]).to be_empty
+    end
+
+    it 'returns an array with the options and the params' do
+      expect(url_params.keys).to have(7).items
+    end
+
+    it 'has any user specified params' do
+      expect(url_params.keys).to include(:user_specified_param)
     end
   end
 
-  describe "#url_params" do
-    let(:url_params) { helper.url_params }
-    it "returns an array with the options and the params" do
-      url_params.should have(7).params
-    end
-
-    it "has the user-specified params" do
-      url_params.keys.should include(:my_special_param)
-    end
-  end
-
-  describe "#signature_base" do
+  describe '#signature_base' do
     let(:signature_base) { helper.signature_base }
-    it "returns an escaped string with method" do
-      signature_base.should match(/GET/)
+
+    it { expect(signature_base).to be_a(String) }
+
+    it 'contains the http method' do
+      expect(signature_base).to match(/GET/)
     end
 
-    it "returns an escaped string with url" do
-      signature_base.should match(/example.com/)
+    it 'contains the url' do
+      expect(signature_base).to match(/example.com/)
     end
 
-    it "returns an escaped string with the params" do
-      signature_base.should match(/oauth_consumer_key/)
-      signature_base.should match(/my_special_param/)
+    it 'contains the params' do
+      expect(signature_base).to match(/oauth_consumer_key/)
+      expect(signature_base).to match(/user_specified_param/)
     end
   end
 
-  describe "#full_url" do
+  describe '#full_url' do
     let(:full_url) { helper.full_url }
-    it "returns the url with the params for auth" do
-      full_url.should match(/oauth_consumer_key=#{consumer_key}/)
-      full_url.should match(/oauth_signature_method=HMAC-SHA1/)
-      full_url.should match(/oauth_signature=/)
-      full_url.should match(/my_special_param/)
-    end
+
+    it { expect(full_url).to match(/oauth_consumer_key=#{consumer_key}/) }
+    it { expect(full_url).to match(/oauth_signature_method=HMAC-SHA1/) }
+    it { expect(full_url).to match(/oauth_signature=/) }
+    it { expect(full_url).to match(/user_specified_param/) }
   end
 end
